c1e5a8e76ad1de7ce0866110eb81d0d0c634a440
techtig <techtig9@gmail.com>
2026-08-12 03:35:44 -0700

    Create loading.tsx

---
+++ ufo/app/dashboard/projects/[id]/loading.tsx
@@ -0,0 +1,11 @@
+export default function ProjectDetailLoading() {
+  return (
+    <div className="mx-auto max-w-6xl space-y-6 p-4">
+      <div className="shimmer h-8 w-48" />
+      <div className="grid gap-4 lg:grid-cols-[240px_1fr]">
+        <div className="shimmer h-96" />
+        <div className="shimmer h-96" />
+      </div>
+    </div>
+  );
+}
